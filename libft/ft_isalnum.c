d6611543375c081aa40aab12f26df6de31b85a92
Hellom Oliveira Mendes <heolivei@c1r13s16.42porto.com>
2022-11-10 16:59:21 +0000

    corrige erros de algumas funcoes

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 11:24:36 by heolivei          #+#    #+#             */
-/*   Updated: 2022/11/06 13:38:52 by heolivei         ###   ########.fr       */
+/*   Updated: 2022/11/10 16:50:27 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalnum(char c)
+int	ft_isalnum(int c)
 {
-	return(ft_isdigit(c) || ft_isalpha)
+	return (ft_isdigit(c) || ft_isalpha(c));
 }
