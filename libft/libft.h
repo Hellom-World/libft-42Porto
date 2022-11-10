59c752bed43d43ac22913c543a2156b0ac4a3779
Hellom-World <hellom-mendes@hotmail.com>
2022-11-10 12:44:49 +0000

    adiciona funçoes isalnum, isascii e isprint

--- libft/libft.h
+++ libft/libft.h
@@ -25,6 +25,8 @@ size_t	ft_strlen(const char *s);
 int		ft_atoi(char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
 
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 
