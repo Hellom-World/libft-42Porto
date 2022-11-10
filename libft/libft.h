846a94eade80459d7e17e9e5e5ca337037d0aae5
Hellom Oliveira Mendes <heolivei@c1r13s16.42porto.com>
2022-11-10 19:07:54 +0000

    acrescenta ft_memset.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: heolivei <heolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 14:43:48 by heolivei          #+#    #+#             */
-/*   Updated: 2022/11/09 20:33:45 by heolivei         ###   ########.fr       */
+/*   Updated: 2022/11/10 18:56:02 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 
+void	*ft_memset(void *str, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
