2bbd619d94d4c6fed757db2e3618c977db13cae3
Hellom-World <hellom-mendes@hotmail.com>
2022-11-11 12:55:03 +0000

    adiciona ft_bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heolivei <heolivei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 18:14:13 by heolivei          #+#    #+#             */
-/*   Updated: 2022/11/08 19:04:28 by heolivei         ###   ########.fr       */
+/*   Created: 2022/11/03 11:24:36 by heolivei          #+#    #+#             */
+/*   Updated: 2022/11/06 13:38:52 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memcpy(void *dst, const void *src, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-	
-	if (!dst && !src)
-		return (0);
-	i = 0;
-
-	while (i < n)
-	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dst);
+	ft_memset(s, '\0', n);
 }
