deae963c6cea81fa82662665f433d9f4de1a9b44
Hellom Oliveira Mendes <heolivei@c1r8s10.42porto.com>
2022-11-17 15:07:34 +0000

    acrescenta isprint

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: heolivei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:08:05 by heolivei          #+#    #+#             */
-/*   Updated: 2022/11/16 15:04:21 by heolivei         ###   ########.fr       */
+/*   Updated: 2022/11/17 14:11:45 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	ucs1 = (unsigned char *)s1;
 	ucs2 = (unsigned char *)s2;
 	i = 0;
-	while (ucs1[i] == ucs2[i])
+	while (i < n)
 	{
-		if (ucs1[i] && ucs2[i] == '\0')
-			return (0);
-		if (n == 0)
-			return (0);
+		if (ucs1[i] != ucs2[i])
+			return (ucs1[i] - ucs2[i]);
 		i++;
-		n--;
 	}
-	return ((int)(ucs1[i] - ucs2[i]));
+	return (0);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: heolivei <heolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 14:43:48 by heolivei          #+#    #+#             */
-/*   Updated: 2022/11/16 14:59:18 by heolivei         ###   ########.fr       */
+/*   Updated: 2022/11/17 14:51:07 by heolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,12 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 void	*ft_memset(void *str, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
