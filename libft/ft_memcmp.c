cc6dcb49ee1d4db9bbe77b30337a95e2e2ba286b
Hellom-World <hellom-mendes@hotmail.com>
2022-11-17 11:57:55 +0000

    correção de teste e funçao memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -18,14 +18,17 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*ucs1;
 	unsigned char	*ucs2;
 
-	ucs1 = unsigned char *s1;
-	ucs2 = unsigned char *s2;
+	ucs1 = (unsigned char *)s1;
+	ucs2 = (unsigned char *)s2;
 	i = 0;
 	while (ucs1[i] == ucs2[i])
 	{
 		if (ucs1[i] && ucs2[i] == '\0')
 			return (0);
+		if (n == 0)
+			return (0);
 		i++;
+		n--;
 	}
-	return ((void *)(ucs1 + i) - (void *)(ucs2 + i));
+	return ((int)(ucs1[i] - ucs2[i]));
 }
