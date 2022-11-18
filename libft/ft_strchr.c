e2019c4b34adeb1e27d09f5d73260a9790b3c18c
Hellom-World <hellom-mendes@hotmail.com>
2022-11-18 00:31:50 +0000

    acrescenta mais algumas funcoes str

---
+++ libft/ft_strchr.c
@@ -0,0 +1,16 @@
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+	i = 0;
+
+	while (s[i] != '\0' && s[i] != c) 
+	{
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
+	return (0);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -29,6 +29,11 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
 
 void	*ft_memset(void *str, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
